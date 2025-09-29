ebd9aa888955a609f32a3ac972dc53ef76f0bc88
madortill <mador.till@gmail.com>
2025-09-29 20:03:47 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/lomdat-safety-instructions/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/BloodDonation/" : "/",
   plugins: [react()],
 })
