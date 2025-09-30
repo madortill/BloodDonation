ad2ee678a06e3fb791052bb472e7b83f55e360fe
madortill <mador.till@gmail.com>
2025-09-30 11:02:57 +0300

    no second graph

--- src/components/BloodEqualsTime.jsx
+++ src/components/BloodEqualsTime.jsx
@@ -1,26 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "../css/BloodEqualsTime.css";
 import one from "../assets/images/graph/one.png";
-import two from "../assets/images/graph/two.png";
 import maskana from "../assets/images/graph/conclusion.svg";
 import maskanaExplain from "../assets/images/graph/conclusion-explain.svg";
 import GraphCredit from "./GraphCredit";
 import arrow from "../assets/images/arrow.png";
 
-const info = [
-  {
+const info = {
+ 
     title: "גרף א'",
     img: one,
     explain:
       "ניתן לראות שבקרב פצועים שקיבלו מוצרי דם - תוך פחות מ-15 דקות מרגע הגעת צוות פינוי אווירי, לעומת אלה שקיבלו תוך מעל 15 דקות - הייתה פחות תמותה ב-24 שעות הראשונות לאחר הפציעה. כלומר, הם שרדו יותר.",
-  },
-  {
-    title: "גרף ב'",
-    img: two,
-    explain:
-      "החוקרים בדקו את שיעורי התמותה לאחר 15 דקות מהגעת צוות פינוי אווירי. ניתן לראות שאין הבדל בתמותה בין פצועים שקיבלו מוצרי דם תוך 16-20 דקות ומעל ל-20 דקות מרגע הגעת צוות פינוי אווירי.",
-  },
-];
+ 
+};
 
 function BloodEqualsTime({ indexSubTitle, setFinishMaskana, setShowNextBtn }) {
   const [chosenBtn, setChosenBtn] = useState("one");
@@ -36,14 +29,14 @@ function BloodEqualsTime({ indexSubTitle, setFinishMaskana, setShowNextBtn }) {
 
   return (
     <>
-      {indexSubTitle !== 3 && (
+      {/* {indexSubTitle !== 3 && (
         <p className="instruction-one">- לחצו כדי לעיין בגרפים -</p>
-      )}
+      )} */}
 
       <div className="blood-equals-time">
         {indexSubTitle !== 3 && (
           <>
-            <div className="graph-btn-container">
+            {/* <div className="graph-btn-container">
               <div
                 className={`graph-btn ${chosenBtn === "one" ? "chosen" : ""}`}
                 onClick={() => setChosenBtn("one")}
@@ -56,17 +49,17 @@ function BloodEqualsTime({ indexSubTitle, setFinishMaskana, setShowNextBtn }) {
               >
                 גרף ב'
               </div>
-            </div>
+            </div> */}
 
             <div className="graph-container">
               <GraphCredit />
-              <p className="graph-header">{info[indexGraph].title}</p>
+              {/* <p className="graph-header">{info.title}</p> */}
               <img
-                src={info[indexGraph].img}
+                src={info.img}
                 alt="graph"
                 className="graph-img"
               />
-              <p className="graph-text">{info[indexGraph].explain}</p>
+              <p className="graph-text">{info.explain}</p>
             </div>
           </>
         )}
